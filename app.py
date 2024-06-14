4deb898b9db50bb97d1bcf6923a6875bfd71c7d8
Dinar Nur Aziz <dinarnuraziz@gmail.com>
2024-06-14 23:25:56 +0700

    add swagger for api spec

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_swagger_ui import get_swaggerui_blueprint
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import NearestNeighbors
@@ -31,14 +32,18 @@ def recommend_books():
     favorite_books = data['favorite_books']
     n_recommendations = data.get('n_recommendations', 5)
     
-    book_indices = [df[df['Book-Title'] == book].index[0] for book in favorite_books]
+    book_indices = [df[df['ISBN'] == book].index[0] for book in favorite_books]
     mean_feature_vector = np.mean(features.iloc[book_indices, :], axis=0).values.reshape(1, -1)
     distances, indices = knn.kneighbors(mean_feature_vector, n_neighbors=n_recommendations + 1)
     
-    recommended_books = [df.iloc[indices[0][i]]['Book-Title'] for i in range(1, len(indices[0]))]
+    recommended_books = [df.iloc[indices[0][i]]['ISBN'] for i in range(1, len(indices[0]))]
     
     return jsonify(recommended_books)
-
+# Swagger UI setup
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yaml'
+swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "Book Recommendation API"})
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
